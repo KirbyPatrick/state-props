93bf7ddce449b36a8ab7093e30347c8728da74ad
Patrick Kirby <pkirbyinatx@gmail.com>
2020-08-25 11:05:51 -0500

    began adding state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 //import components
 import Nav from "./components/Nav";
 import Tweets from "./components/Tweets";
 
 function App() {
+  //State
+  const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  //Functions
+  const incrementer = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+
+  const clear = () => {
+    setCounter(counter == 0);
+  };
+
+  const toggler = () => {
+    setToggle((prev) => !prev);
+  };
+
+  //Render
   return (
     <div className="App">
+      <h1 className={toggle ? "active" : ""}>Hello React</h1>
+      <h2>Counter {counter}</h2>
+      <button onClick={incrementer}>Add</button>
+      <button onClick={clear}>Clear</button>
+      <button onClick={toggler}>Toggle</button>
       <div className="home">
         <Nav />
         <Tweets />
